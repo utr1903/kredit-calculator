4f94746d89ab243d7a6b65e945a397f385e10b65
uturkarslan <ugurturkarslan@gmail.com>
2023-07-28 19:48:38 +0200

    Implement logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import yaml
 import math
 import matplotlib.pyplot as plt
@@ -14,60 +15,72 @@ with open("./config.yaml", "r") as stream:
     print(exc)
     exit(1)
 
-print()
-print('-------------')
-print('--- INPUT ---')
-print('-------------')
-print()
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+loglevel = config['loglevel']
+
+match loglevel:
+  case "DEBUG":
+    print("DEBUG")
+    logger.setLevel(level=logging.DEBUG)
+  case _:
+    print("INFO")
+    logger.setLevel(level=logging.INFO)
+
+logger.info('')
+logger.info('-------------')
+logger.info('--- INPUT ---')
+logger.info('-------------')
+logger.info('')
 
 # Kapital
 eigenkapital = config['eigenkapital']
-print("eigenkapital: " + str(config['eigenkapital']))
+logger.info("eigenkapital: " + str(config['eigenkapital']))
 kaufpreis = config['kaufpreis']
-print("kaufpreis: " + str(config['kaufpreis']))
+logger.info("kaufpreis: " + str(config['kaufpreis']))
 modernisierung = config['modernisierung']
-print("modernisierung: " + str(config['modernisierung']))
+logger.info("modernisierung: " + str(config['modernisierung']))
 
 # Nebenkosten
 grunderwerbsteuer = config['grunderwerbsteuer']
-print("grunderwerbsteuer: " + str(config['grunderwerbsteuer']))
-print("notar: " + str(config['notar']))
+logger.info("grunderwerbsteuer: " + str(config['grunderwerbsteuer']))
+logger.info("notar: " + str(config['notar']))
 notar = config['notar']
 makler = config['makler']
-print("makler: " + str(config['makler']))
+logger.info("makler: " + str(config['makler']))
 
 # Kredit
 annuitat = config['annuitat']
-print("annuitat: " + str(config['annuitat']))
+logger.info("annuitat: " + str(config['annuitat']))
 zinssatzJahrlich = config['zinssatzJahrlich']
-print("zinssatzJahrlich: " + str(config['zinssatzJahrlich']))
+logger.info("zinssatzJahrlich: " + str(config['zinssatzJahrlich']))
 zinsbindungJahre = config['zinsbindungJahre']
-print("zinsbindungJahre: " + str(config['zinsbindungJahre']))
+logger.info("zinsbindungJahre: " + str(config['zinsbindungJahre']))
 
 ##################
 ### Berechnung ###
 ##################
 
-print()
-print('------------------')
-print('--- BERECHNUNG ---')
-print('------------------')
-print()
+logger.info('')
+logger.info('------------------')
+logger.info('--- BERECHNUNG ---')
+logger.info('------------------')
+logger.info('')
 
 nebenkosten = kaufpreis * (grunderwerbsteuer + notar + makler)
-print("nebenkosten: " + str(nebenkosten))
+logger.info("nebenkosten: " + str(nebenkosten))
 darlehen = kaufpreis + modernisierung + nebenkosten - eigenkapital
-print("darlehen: " + str(darlehen))
+logger.info("darlehen: " + str(darlehen))
 
 zinssatzMonatlich = zinssatzJahrlich / 12.0
 q = 1.0 + zinssatzMonatlich
 
 laufzeitMonaten = -math.log(1.0 - (darlehen * zinssatzMonatlich / annuitat)) / math.log(q)
-print("laufzeitMonaten: " + str(laufzeitMonaten))
-print("laufzeitJahre: " + str(laufzeitMonaten / 12.0))
+logger.info("laufzeitMonaten: " + str(laufzeitMonaten))
+logger.info("laufzeitJahre: " + str(laufzeitMonaten / 12.0))
 
 anfangstilgung = annuitat * 12.0 / darlehen - zinssatzMonatlich
-print("anfangstilgung: " + str(anfangstilgung))
+logger.info("anfangstilgung: " + str(anfangstilgung))
 
 # Plot variables
 zinsbindungMonate = zinsbindungJahre * 12.0
@@ -87,19 +100,19 @@ immobilienEigentumTotal = eigenkapital
 
 for monat in range(int(laufzeitMonaten)):
 
-  print("---")
+  logger.debug("---")
 
   # Time
   jahr = monat / 12.0
   jahre.append(jahr)
-  print("jahr: " + str(jahr))
+  logger.debug("jahr: " + str(jahr))
 
   restschuld = darlehen * pow(q, monat) - annuitat * (pow(q, monat) - 1.0) / zinssatzMonatlich
-  print("restschuld: " + str(restschuld))
+  logger.debug("restschuld: " + str(restschuld))
   zins = darlehen * (pow(q, laufzeitMonaten) - pow(q, monat - 1.0)) / (pow(q, laufzeitMonaten) - 1.0) * zinssatzMonatlich
-  print("zins: " + str(zins))
+  logger.debug("zins: " + str(zins))
   tilgung = darlehen * pow(q, monat - 1.0) * zinssatzMonatlich / (pow(q, laufzeitMonaten) - 1.0)
-  print("tilgung: " + str(tilgung))
+  logger.debug("tilgung: " + str(tilgung))
 
   zinsTotal = zinsTotal + zins
   tilgungTotal = tilgungTotal + tilgung
@@ -119,20 +132,20 @@ for monat in range(int(laufzeitMonaten)):
   immobilienEigentumTotal = immobilienEigentumTotal + tilgung
   immobilienEigentum.append(immobilienEigentumTotal / (kaufpreis + nebenkosten) * 100)
 
-print()
-print('----------------')
-print('--- ERGEBNIS ---')
-print('----------------')
-print()
+logger.info('')
+logger.info('----------------')
+logger.info('--- ERGEBNIS ---')
+logger.info('----------------')
+logger.info('')
 
-print("zinsBezahlt: " + str(zinsBezahlt))
-print("tilgungBezahlt: " + str(tilgungBezahlt))
+logger.info("zinsBezahlt: " + str(zinsBezahlt))
+logger.info("tilgungBezahlt: " + str(tilgungBezahlt))
 immobilienEigentumAmZinsbindung = eigenkapital + tilgungBezahlt
 immobilienEigentumAmZinsbindungProzent = immobilienEigentumAmZinsbindung / (kaufpreis + nebenkosten) * 100
-print("immobilienEigentumAmZinsbindung: " + str(immobilienEigentumAmZinsbindung))
-print("immobilienEigentumAmZinsbindungProzent: " + str(immobilienEigentumAmZinsbindungProzent))
+logger.info("immobilienEigentumAmZinsbindung: " + str(immobilienEigentumAmZinsbindung))
+logger.info("immobilienEigentumAmZinsbindungProzent: " + str(immobilienEigentumAmZinsbindungProzent))
 
-print("---")
+logger.info("---")
 
 ######################
 ### Visualisierung ###
@@ -155,6 +168,7 @@ plt.xlabel('Jahre')
 
 plt.grid(visible=True)
 plt.ylim(0.0, annuitat * 1.1)
+plt.axvline(x = zinsbindungJahre, linestyle='--', color = 'r', label = 'Zinsbindung')
 plt.axhline(y = annuitat, linestyle='--', color = 'g', label = 'Annuitat')
 plt.plot(jahre, zinsen, label="Zinsen")
 plt.plot(jahre, tilgungen, label="Tilgung")
